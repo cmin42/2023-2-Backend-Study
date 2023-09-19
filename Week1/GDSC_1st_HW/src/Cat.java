d8d99a08c7adc3310a04744c3401064d940e223b
cmin42 <129381643+cmin42@users.noreply.github.com>
2023-09-19 23:10:38 +0900

    Week1 과제 제출

---
+++ Week1/GDSC_1st_HW/src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat extends Animal {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println(this.getName() + " says Meow!");
+    }
+}
---
+++ Week1/GDSC_1st_HW/src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog extends Animal {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println(this.getName() + " says Woof!");
+    }
+}
